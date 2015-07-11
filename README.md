95a2c1d20c4282725d853415ac8481cb20a36a5a
Joseph Holland <info@josephholland.ie>
2015-07-11 01:34:13 +0100

    Updated script to contain error handling.

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ Python script to listen for LightwaveRF energy monitor values and input into Emo
 
 This is a simple Python script that I put together to help input energy usage data from a LightwaveRF energy monitor into Emoncms (http://emoncms.org) where I can create dashboards and run reports on power usage, etc.  In order to use this you will need both a LightwaveRF WiFiLink and energy monitor devices (http://lightwaverf.com).
 
-This script listens on UDP port 9761 for the output of the energy monitor device and then parses the data and using the Emoncms API inputs it as a feed.  You will need to create an Emoncms API key to be able to write into a feed.  I run this then from using a screen session in Linux.
+This script listens on UDP port 9761 for the output of the energy monitor device and then parses the data and using the Emoncms API inputs it as a feed.  You will need to create an Emoncms API key to be able to write into a feed.  I run this then from using a screen session in Linux, but there's now reason it could not be run in Windows.
+
+By the way to code for inputting feeds was based off of the emoncms pylink script here: https://github.com/emoncms/development/blob/master/Tutorials/Python/PyLink/pylink.py
 
 The Python may not be that great as this is my first foray into it, so I do apologise...  I'll update this over time if needed.
--- lwrf_pylink.py
+++ lwrf_pylink.py
@@ -5,54 +5,78 @@ import httplib
 import requests
 import socket
 
+# Define IP address and port to listen on.
+# UDP 9761 is the default for this on the WiFiLink.
 UDP_IP = "0.0.0.0"
 UDP_PORT = 9761
 
-# Domain you want to post to: localhost would be an emoncms installation on your own laptop
-# this could be changed to emoncms.org to post to emoncms.org
-host = "localhost"
+# Host you want to post to: localhost would be an
+# emoncms installation on this computer, this could
+# be changed to emoncms.org to post to emoncms.org.
+host = "<host>"
 
-# Location of emoncms in your server, the standard setup is to place it in a folder called emoncms
+# Location of emoncms install on your server, the 
+# standard setup is to place it in a folder called emoncms
 # To post to emoncms.org change this to blank: ""
 emoncmspath = "emoncms"
 
-# Write apikey of emoncms account
-apikey = "<emoncms_api_key>"
+# Write the apikey of emoncms account.
+apikey = "<apikey>"
 
-# Node id youd like the emontx to appear as
+# Node id youd like the feed to appear from.
 nodeid = 1
 
+# Define HTTP connection to emoncms host.
 conn = httplib.HTTPConnection(host)
 
+# Start loop to listen for UDP packets from the
+# LightwaveRF WiFiLink and once recieved, clean
+# the data up and send to emoncms.
 while True:
-  # Read in line of readings from emontx serial
-  sock = socket.socket(socket.AF_INET, # Internet
-                     socket.SOCK_DGRAM) # UDP
-  sock.bind((UDP_IP, UDP_PORT))
+  try:
+    # Read in line of readings from UPD port.
+    sock = socket.socket(socket.AF_INET, # Internet
+                       socket.SOCK_DGRAM) # UDP
+    sock.bind((UDP_IP, UDP_PORT))
 
-  data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-  if "pwrMtr" in data:
-    print "Received data from UDP"
-    print data
+    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+  except Exception, msg:
+    print "Error capturing UDP packets."
+    print msg
+  else:
+    # We've got a UDP packet successfully, do some checking
+    # and processing and then send to emoncms.
 
-    # Split the line at the comma
-    array = data.split(',')
-    #  array2 = data.split(':')
-    #print array[7]
+    # Ensure the data has pwrMtr in the string captured.
+    # We need to do this otherwise we would catch the 
+    # other command packets here too, i.e. turn on light X.
+    if "pwrMtr" in data:
+      # Some logging to console.  I run this via screen
+      # and this suits best instead of sending to a log.
+      print "Received data from UDP"
+      print data
 
-    # split element in list again by :
-    cUse = array[7].split(':')
+      # Split the line at the comma
+      array = data.split(',')
 
-    # print only power usage
-    Watts = cUse[1]
-    print "Current Watts: "+str(Watts)
+      # split element in list again by ":"
+      cUse = array[7].split(':')
 
-    # Send to emoncms
-    print "Send data to EmonCMS"
-    conn.request("GET", "/"+emoncmspath+"/input/post.json?apikey="+apikey+"&node="+str(nodeid)+"&json={Watts:"+str(Watts)+"}")
+      # Print only power usage to console.
+      Watts = cUse[1]
+      print "Current Watts: "+str(Watts)
 
-    # Get response
-    response = conn.getresponse()
-    print "Response"
-    print response.status, response.reason
-    conn.close()
+      try:
+        # Send to emoncms.
+        print "Send data to EmonCMS"
+        conn.request("GET", "/"+emoncmspath+"/input/post.json?apikey="+apikey+"&node="+str(nodeid)+"&json={Watts:"+str(Watts)+"}")
+
+        # Get response.
+        response = conn.getresponse()
+        print "Response"
+        print response.status, response.reason
+        # Important ro remember to close the connection!
+        conn.close()
+      except Exception, msg:
+        print "Error sending to emoncms."
+        print msg
